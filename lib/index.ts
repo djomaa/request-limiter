e54e7fa3ef8f4514f943307ed524a6ce50373bae
Demid Mosko <d.mosko@pixelplex.by>
2018-12-11 19:30:42 +0300

    add bases

--- lib/index.ts
+++ lib/index.ts
@@ -1,18 +1,37 @@
+import Timeout = NodeJS.Timeout;
+
 export const ERROR = {
 	NOT_A_PROMISE_FUNCTION: 'function should return promise',
 };
 
-export default class RequestLimiter {
-	private _queue: Array<{
-		func: () => Promise<any>,
-		resolve: (result: any) => void,
-		reject: (error: Error | any) => void,
-	}> = [];
-	private _activeNumber: number = 0;
+function retryDelayFormula(index: number): number {
+	return 10 * (2 ** index);
+}
 
+type JobFunction = () => Promise<any>;
+type Job = {
+	func: JobFunction,
+	resolve: (result: any) => void,
+	reject: (error: Error | any) => void,
+}
+
+export default class RequestLimiter {
+	private _queue: Array<Job> = [];
+	private _activeNumber: number = 0;
+	private _retryTimeoutTimestamp: number|null = null;
+	private _retryTimeout: Timeout|null = null;
+	private _retriesCounter: Array<{ func: JobFunction, index: number }>;
+	private _blocked: boolean = false;
 	public get length() : number { return this._queue.length; }
 
-	constructor(public maxRequests: number, public autoStart: boolean = true) {}
+	constructor(
+		public maxRequests: number,
+		public autoStart: boolean = true,
+		public retry: {
+			maxCount: number,
+			delayFormula: null|((index: number) => number),
+		} = { maxCount: 0, delayFormula: retryDelayFormula },
+	) {}
 
 	public add<T>(func: () => Promise<T>) : Promise<T>{
 		return new Promise((resolve, reject) => {
@@ -23,12 +42,40 @@ export default class RequestLimiter {
 		});
 	}
 
-	private async _process({ func, resolve, reject}) {
+	private _retryJob(job: Job, error: Error) {
+		const index = this._increaseRetryIndex(job.func);
+		if (index > this.retry.maxCount) {
+			job.reject(error);
+			return;
+		}
+		if (this.retry.delayFormula) {
+			this._blocked = true;
+			const now = (new Date).getTime();
+			if (this._retryTimeout) {
+				if (this._retryTimeoutTimestamp < now) return;
+				clearTimeout(this._retryTimeout);
+			}
+			const delay = this.retry.delayFormula(index);
+			this._retryTimeout = setTimeout(() => { this._blocked = false }, delay);
+		}
+		this._queue.push(job);
+	}
+
+	private _increaseRetryIndex(func: JobFunction) : number {
+		const counter = this._retriesCounter.find(({ func: aFunc }) => aFunc === func);
+		if (!counter) this._retriesCounter.push({ func, index: 1 });
+		else counter.index += 1;
+		return counter ? counter.index : 1;
+
+	}
+
+	private async _process({ func, resolve, reject}: Job) {
 		try {
 			const promise = func();
 			if (!(promise instanceof Promise)) throw new Error(ERROR.NOT_A_PROMISE_FUNCTION);
 			resolve(await promise);
 		} catch (error) {
+			this._retryJob({ func, resolve, reject }, error);
 			reject(error);
 		} finally {
 			this._activeNumber -= 1;

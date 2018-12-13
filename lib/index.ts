07f79116cd4aa918e7c0be548e21d67d4afca97e
Demid Mosko <d.mosko@pixelplex.by>
2018-12-13 12:49:15 +0300

    tests

--- lib/index.ts
+++ lib/index.ts
@@ -18,19 +18,26 @@ type Job = {
 export default class RequestLimiter {
 	private _queue: Array<Job> = [];
 	private _activeNumber: number = 0;
-	private _retryTimeoutTimestamp: number|null = null;
-	private _retryTimeout: Timeout|null = null;
-	private _retriesCounter: Array<{ func: JobFunction, index: number }>;
 	private _blocked: boolean = false;
+	private _retry: {
+		timeoutEndTimestamp: number|null,
+		timeout: Timeout|null,
+		counter: Array<{ func: JobFunction, index: number }>,
+	} = {
+		timeoutEndTimestamp: null,
+		timeout: null,
+		counter: [],
+	};
 	public get length() : number { return this._queue.length; }
 
 	constructor(
 		public maxRequests: number,
 		public autoStart: boolean = true,
 		public retry: {
-			maxCount: number,
-			delayFormula: null|((index: number) => number),
-		} = { maxCount: 0, delayFormula: retryDelayFormula },
+			maxCount?: number,
+			blockDelayFormula?: null|((index: number) => number),
+			immediately?: boolean,
+		} = { maxCount: 0, blockDelayFormula: retryDelayFormula, immediately: false },
 	) {}
 
 	public add<T>(func: () => Promise<T>) : Promise<T>{
@@ -42,41 +49,46 @@ export default class RequestLimiter {
 		});
 	}
 
-	private _retryJob(job: Job, error: Error) {
+	private _handleError(job: Job, error: Error) {
 		const index = this._increaseRetryIndex(job.func);
 		if (index > this.retry.maxCount) {
 			job.reject(error);
-			return;
-		}
-		if (this.retry.delayFormula) {
+		} else if (this.retry.immediately) {
+			this._processJob(job);
+		} else if (this.retry.blockDelayFormula) {
 			this._blocked = true;
 			const now = (new Date).getTime();
-			if (this._retryTimeout) {
-				if (this._retryTimeoutTimestamp < now) return;
-				clearTimeout(this._retryTimeout);
+			if (this._retry.timeout) {
+				if (this._retry.timeoutEndTimestamp < now) return;
+				clearTimeout(this._retry.timeout);
 			}
-			const delay = this.retry.delayFormula(index);
-			this._retryTimeout = setTimeout(() => { this._blocked = false }, delay);
+			const delay = this.retry.blockDelayFormula(index);
+			this._retry.timeoutEndTimestamp = now + delay;
+			this._retry.timeout = setTimeout(() => {
+				this._retry.timeout = null;
+				this._blocked = false;
+				this.start();
+			}, delay);
+			this._queue.push(job);
 		}
-		this._queue.push(job);
 	}
 
 	private _increaseRetryIndex(func: JobFunction) : number {
-		const counter = this._retriesCounter.find(({ func: aFunc }) => aFunc === func);
-		if (!counter) this._retriesCounter.push({ func, index: 1 });
+		const counter = this._retry.counter.find(({ func: aFunc }) => aFunc === func);
+		if (!counter) this._retry.counter.push({ func, index: 1 });
 		else counter.index += 1;
 		return counter ? counter.index : 1;
 
 	}
 
-	private async _process({ func, resolve, reject}: Job) {
+	private async _processJob({ func, resolve, reject}: Job) {
 		try {
+			this._activeNumber += 1;
 			const promise = func();
 			if (!(promise instanceof Promise)) throw new Error(ERROR.NOT_A_PROMISE_FUNCTION);
 			resolve(await promise);
 		} catch (error) {
-			this._retryJob({ func, resolve, reject }, error);
-			reject(error);
+			this._handleError({ func, resolve, reject }, error);
 		} finally {
 			this._activeNumber -= 1;
 			this.start();
@@ -84,11 +96,11 @@ export default class RequestLimiter {
 	}
 
 	public start(): Promise<void[]> {
+		if (this._blocked) return;
 		if (this._queue.length === 0) return;
 		const toExecute = this._queue.splice(0, this.maxRequests - this._activeNumber);
 		return Promise.all(toExecute.map((job) => {
-			this._activeNumber += 1;
-			return this._process(job);
+			return this._processJob(job);
 		}));
 	}
 
--- utils/common.js
+++ utils/common.js
@@ -12,6 +12,7 @@ const randomInt = (to, from = 0) => {
  * creates new array with passed size using the function to fill. Can be used
  * @param {Object|Number} options options object or number (size)
  * @param {Number?} options.size size of the array
+ * @param {Boolean?} options.passArray pass array to the func
  * @param {Boolean?} options.await if true the function returns Promise.all(resultArray)
  * @param {Function|String} func function or string used size times. Can be async
  * @return {Array|Promise<Array>}
@@ -21,14 +22,51 @@ function fillArray(options, func) {
 		options = { size: options };
 	}
 	const array = new Array(options.size);
+	if (options.passArray) {
+		if (typeof func !== 'function') throw new Error('can not pass array to not a function');
+		func = func.bind(null, array);
+	}
 	for (let i = 0; i < options.size; i += 1) {
-		array[i] = typeof func === 'function' ? func() : func;
+		array[i] = typeof func === 'function' ? func(array) : func;
 	}
 	return options.await ? Promise.all(array) : array;
 }
 
+function findMin(array, { returnIndex } = {}) {
+	let min = array[0];
+	let index = 0;
+	for (let i = 1; i < array.length; i += 1) {
+		if (min > array[i]) continue;
+		min = array[i];
+		index = i;
+	}
+	return returnIndex ? [min, index] : min;
+}
+
+function findMax(array, { returnIndex } = {}) {
+	let max = array[0];
+	let index = 0;
+	for (let i = 1; i < array.length; i += 1) {
+		if (max > array[i]) continue;
+		max = array[i];
+		index = i;
+	}
+	return returnIndex ? [max, index] : max;
+}
+
+/**
+ * returns an random element of the passed array
+ * @param {Array} array
+ * @return {*} random element of the array
+ */
+function getRandomArrayItem(array) {
+	return array[randomInt(array.length - 1)];
+}
 
 module.exports = {
 	randomInt,
 	fillArray,
+	getRandomArrayItem,
+	findMin,
+	findMax,
 };
